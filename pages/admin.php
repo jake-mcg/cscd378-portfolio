66ef86627cd827650ec56b455552ed7c82f6c427
nnoose <91214665+nnoose@users.noreply.github.com>
2022-12-02 13:19:05 -0800

    fixed typo.

--- pages/admin.php
+++ pages/admin.php
@@ -1,6 +1,6 @@
 <?php require_once "header.php";
 
-#if the useer isn't logged in, tell them that they can't be here.
+#if the user isn't logged in, tell them that they can't be here.
 if (!$auth->isLogged()) {
     header('HTTP/1.0 403 Forbidden');
     echo "Forbidden";
