179a46f9633961ae7f34bbbd3ee1f629eb08b964
nnoose <91214665+nnoose@users.noreply.github.com>
2022-12-02 14:44:06 -0800

    added contact database handling to cookieForm.php.

--- pages/CookieTest.php
+++ pages/CookieTest.php
@@ -1,15 +1,16 @@
-<?php require_once "header.php"; ?>
+<?php
+require_once "header.php";
 
-    <?php
-    if(isset($_COOKIE["firstName"]) && isset($_COOKIE["lastName"]) && isset($_COOKIE["companyName"])) {
-        echo "welcome " . $_COOKIE["firstName"].  " " . $_COOKIE["lastName"]."<br>" ;
-        echo "from ".$_COOKIE["companyName"]."<br>" ;
-        echo "your cookie was created on ";
-        echo $_COOKIE["timestamp"];
-    }
-    else{
-        header('location:cookieForm.php');
-    }
-    ?>
 
-<?php require_once "footer.php"; ?>
+if(isset($_COOKIE["firstName"]) && isset($_COOKIE["lastName"]) && isset($_COOKIE["companyName"])) {
+    echo "welcome " . $_COOKIE["firstName"].  " " . $_COOKIE["lastName"]."<br>" ;
+    echo "from ".$_COOKIE["companyName"]."<br>" ;
+    echo "your cookie was created on ";
+    echo $_COOKIE["timestamp"];
+}
+else{
+    header('location:cookieForm.php');
+    exit();
+}
+
+require_once "footer.php"; ?>
