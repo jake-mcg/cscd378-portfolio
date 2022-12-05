6d075c27320c7fd398ab1286d7a78300b7c8e6e2
Ethan <EHowe356@gmail.com>
2022-12-04 16:55:53 -0800

    worked on portfolio

--- pages/ethan.php
+++ pages/ethan.php
@@ -1,13 +1,78 @@
 <?php require_once "header.php"; ?>
 
-
-<div id="container">
-    <div id="row">
-        <div id="col">
-            <p>
-                Ethan's Portfolio
-            </p>
+<hr>
+    <div class="container">
+        <div class="row my-3">
+            <div class="col-3">
+                <img src="images/ethan.webp" alt="Jake Profile Picture" class="img-fluid rounded-start" />
+            </div>
+            <div class="col-9">
+                <h1>Ethan Howe</h1>
+                <h3>EWU Computer Science - Senior</h3>
+                <h5> Gaduation Spring 2023</h5>
+                <p class="text-muted"> I love to learn new things and always strive to improve</p>
+            </div>
         </div>
+        <div class = "row justify-content-evenly">
+            <div class="col-4">
+                <i class="fas fa-envelope fa-fw me-2"></i><a href="mailto:Ehowe356@gmail.com">Ehowe356@gmail.com</a>
+            </div>
+            <div class="col-4">
+                <i class="fab fa-github fa-fw me-2"></i><a href="https://github.com/Ehowe356">github.com/Ehowe356</a>
+            </div>
+        </div>
+        <hr>
+
+        <div class = row g-4>
+            <div class="col">
+                <div class="card" style="border: 1px solid">
+                    <div class="card-header d-flex justify-content-center">
+                        <h4 class="card-title">Work Experience</h4>
+                    </div>
+                    <div class="card-body">
+                        <h5>job 1</h5>
+                        <p>descriptions</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="border: 1px solid">
+                    <div class="card-header d-flex justify-content-center">
+                        <h4 class="card-title">Languages/skills</h4>
+                    </div>
+                    <div class="card-body">
+                        <ul>
+
+                            <li>test1</li>
+                            <li>test2</li>
+                            <li>test3</li>
+                            <li>test3</li>
+                            <li>test3</li>
+                            <li>test3</li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class = "row g-3">
+            <div class="col">
+                <div class="card" style="border: 1px solid">
+                    <div class="card-header d-flex justify-content-center">
+                        <h4 class="card-title">Projects</h4>
+                    </div>
+                    <div class="card-body">
+                        <h5>project 1</h5>
+                        <p>descriptions</p>
+                    </div>
+                </div>
+            </div>
+        </div>>
     </div>
-</div>
+<div class = row g-4></div>
+</body>
 <?php require_once "footer.php"; ?>
+
+
+
+
