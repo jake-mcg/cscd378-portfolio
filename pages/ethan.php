19a7a57353aa177710ad2a54de5f6f58b8b4a76e
Ethan <EHowe356@gmail.com>
2022-12-04 17:27:25 -0800

    finished portfolio

--- pages/ethan.php
+++ pages/ethan.php
@@ -26,12 +26,18 @@
         <div class = row g-4>
             <div class="col">
                 <div class="card" style="border: 1px solid">
-                    <div class="card-header d-flex justify-content-center">
-                        <h4 class="card-title">Work Experience</h4>
+                    <div class="card-header d-flex justify-content-center ">
+                        <h4 class="card-title" >Work Experience</h4>
                     </div>
                     <div class="card-body">
-                        <h5>job 1</h5>
-                        <p>descriptions</p>
+                        <h5>Team Member| September 2018 - August 2022
+                            Kentucky Fried Chicken, Airway Heights, W
+                        </h5>
+                        <p>I worked mainly as A dishwasher. Still, I was able to perform most jobs if needed.</p>
+                        <h5>web Developer |  December 2017 - January 2018 and April 2018
+                            SBA Simplified, Spokane, WA
+                        </h5>
+                        <p>Worked on my Employer's website, performing maintenance, updating systems, taking requests, and fulfilling them professionally and in a timely manner.</p>
                     </div>
                 </div>
             </div>
@@ -43,31 +49,47 @@
                     <div class="card-body">
                         <ul>
 
-                            <li>test1</li>
-                            <li>test2</li>
-                            <li>test3</li>
-                            <li>test3</li>
-                            <li>test3</li>
-                            <li>test3</li>
+                            <li>HTML</li>
+                            <li>Java</li>
+                            <li>Unity</li>
+                            <li>SQL</li>
+                            <li>Android Studio</li>
+                            <li>WordPress</li>
+                            <li>C</li>
+                            <li>Unix</li>
 
                         </ul>
                     </div>
                 </div>
             </div>
         </div>
-        <div class = "row g-3">
+        <div class = "row g-3 mt-2">
             <div class="col">
                 <div class="card" style="border: 1px solid">
                     <div class="card-header d-flex justify-content-center">
                         <h4 class="card-title">Projects</h4>
                     </div>
                     <div class="card-body">
-                        <h5>project 1</h5>
-                        <p>descriptions</p>
+                        <h5>KFC Bucket, December 2022<br>
+                            KFC Reach Grant, Airway Heights, WA
+                        </h5>
+                        <p>This program was created to show off my work as a CS Major. collaborated with another to create a program that uses OpenGL to create a bucket rotating around the y-axis. This program is constantly developing as I continue to learn new skills in OpenGL.
+                        </p>
+                        <h5>Transcript Request Form, September 2019<br>
+                            SVT Design Contest, Spokane, WA
+                        </h5>
+                        <p>This program is a chrome extension that allows students to fill out a form to request a transcript to be sent to the student college of choice. The form is accessed through a chrome extension to be easy to integrate into the current school Chromebook.
+                        </p>
+                        <h5>Fitness Lock, September 2018<br>
+                            SVT Design contest, Spokane, WA
+                        </h5>
+                        <p>This app locks your phone and does not unlock it until you reach a fitness goal. This year-long project had me focus on the agile process. The primary tool I used was android studio to create the environment to test my app.
+                        </p>
+
                     </div>
                 </div>
             </div>
-        </div>>
+        </div>
     </div>
 <div class = row g-4></div>
 </body>
